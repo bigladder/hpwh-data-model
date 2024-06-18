cadc618e52ed4300969fddf56a57423c33a4a654
Phil Ahrenkiel <spahrenk@gmail.com>
2024-06-18 15:22:11 -0600

    Change field to timestamp.

--- build_hpwh.py
+++ build_hpwh.py
@@ -5,14 +5,9 @@ import os
 
 root_dir = "data_model"
 build_dir = "."
-build_out_dir = "out"
+build_out_dir = "../HPWHsim/src/data_model"
 
-lat = Lattice(root_dir, build_dir,  build_out_dir, True)
+lat = Lattice(root_dir, build_dir,  build_out_dir, False)
+
 
 lat.generate_cpp_headers()
-#lat.generate_json_schemas#(build_out_dir, build_out_dir)
-#lat.generate_json_examples()f
-
-#in_file = "Rheem2020Prem50.yaml"
-#out_file = "out.json"
-#lat.do_translate(in_file, out_file)
