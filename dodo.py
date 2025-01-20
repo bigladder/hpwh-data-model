64d873428df004b3bf048c11ee185beff69f7581
Tanaya Mankad <tanaya.mankad@bigladdersoftware.com>
2025-01-20 11:05:33 -0700

    Separate example validation from cpp generation.

--- dodo.py
+++ dodo.py
@@ -7,8 +7,26 @@ from doit.tools import create_folder
 
 from lattice import Lattice
 
-data_model = Lattice()
+data_model = Lattice(build_validation=False)
 
+
+def task_generate_meta_schemas():
+    """Generate JSON meta schemas"""
+    return {
+        "file_dep": [schema.file_path for schema in data_model.schemas],
+        "targets": [schema.meta_schema_path for schema in data_model.schemas],
+        "actions": [(data_model.generate_meta_schemas, [])],
+        "clean": True,
+    }
+
+def task_validate_schemas():
+    """Validate the example schemas against the JSON meta schema"""
+    return {
+        "task_dep": [f"generate_meta_schemas"],
+        "file_dep": [schema.file_path for schema in data_model.schemas]
+        + [schema.meta_schema_path for schema in data_model.schemas],
+        "actions": [(data_model.validate_schemas, [])],
+    }
 
 def task_validate_example_files():
     """Validates the example files against the JSON schema (and other validation steps)"""
@@ -22,6 +40,7 @@ def task_validate_example_files():
 def task_generate_cpp_code():
     """Generate CPP headers and source for example schema."""
     return {
+        "task_dep": [f"validate_schemas"],
         "file_dep": [schema.file_path for schema in data_model.cpp_schemas]
         + [schema.meta_schema_path for schema in data_model.schemas],
         "targets": [schema.cpp_header_file_path for schema in data_model.cpp_schemas]
