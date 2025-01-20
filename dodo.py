4bdacfbbaa4270d66f3667b290eacb0216e2d6ed
Tanaya Mankad <tanaya.mankad@bigladdersoftware.com>
2025-01-20 07:42:50 -0700

    Correct schema syntax, take 1.

--- dodo.py
+++ dodo.py
@@ -14,7 +14,7 @@ def task_validate_example_files():
     """Validates the example files against the JSON schema (and other validation steps)"""
     return {
         "file_dep": data_model.examples
-        + [schema.path for schema in data_model.schemas],
+        + [schema.file_path for schema in data_model.schemas],
         "actions": [(data_model.validate_example_files, [])],
     }
 
@@ -22,10 +22,10 @@ def task_validate_example_files():
 def task_generate_cpp_code():
     """Generate CPP headers and source for example schema."""
     return {
-        "file_dep": [schema.path for schema in data_model.cpp_schemas]
+        "file_dep": [schema.file_path for schema in data_model.cpp_schemas]
         + [schema.meta_schema_path for schema in data_model.schemas],
-        "targets": [schema.cpp_header_path for schema in data_model.cpp_schemas]
-        + [schema.cpp_source_path for schema in data_model.cpp_schemas],
+        "targets": [schema.cpp_header_file_path for schema in data_model.cpp_schemas]
+        + [schema.cpp_source_file_path for schema in data_model.cpp_schemas],
         "actions": [(data_model.generate_cpp_project, [[]])],
         "clean": True,
     }
